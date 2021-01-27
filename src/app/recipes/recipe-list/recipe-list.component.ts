1011da5f0a2cd8bdedb1dd268967a5ff332239b9
Nathan Spinler <nspinler@gmail.com>
2021-01-26 22:24:19 -0600

    Finished CH3

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,10 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  reciept: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', '')
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563'),
+    new Recipe('A Test Recipe 2', 'This is a test 2', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563')
+
   ];
 
   constructor() { 
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+    // short hand for defining the properties and assigning in constructor
+    constructor(public name: string, public amount: number) {};
+}
